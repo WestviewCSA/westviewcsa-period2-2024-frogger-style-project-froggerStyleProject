23a2839df198f206eed8d5507e6726bd69c98774
1923685 <1923685@01-18125.pusd.dom>
2024-12-03 11:28:46 -0800

    needs work

---
+++ FroggerStyleProject/src/Charmander.java
@@ -0,0 +1,182 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Rectangle;
+import java.awt.Toolkit;
+import java.awt.geom.AffineTransform;
+import java.net.URL;
+
+public class Charmander{
+	private Image forward, backward, left, right; 	
+	private AffineTransform tx;
+	
+	int dir = 0; 					//0-forward, 1-backward, 2-left, 3-right
+	int width, height;
+	int x, y;						//position of the object
+	int vx, vy;						//movement variables
+	double scaleWidth = 1.0;		//change to scale image
+	double scaleHeight = 1.0; 		//change to scale image
+
+	public Charmander() {
+		
+		//load the main image (front or forward view) 
+		
+		
+		forward 	= getImage("/imgs/"+"charmander-pixilart (1).png"); //load the image for Tree
+		
+		//alter these
+		width = 60;
+		height = 60;
+		
+		//used for placement on the JFrame
+		x = Frame.width/2;
+		y = Frame.height - height*2;
+		
+		vx = 0;
+		vy = 0;
+		
+		tx = AffineTransform.getTranslateInstance(0, 0);
+		
+		init(x, y); 				//initialize the location of the image
+									//use your variables
+		
+	}
+	public boolean collided(Charmander character) {
+		
+		//represent each object as a rectangle
+		//then check if they are intersecting 
+		Rectangle main = new Rectangle(
+				character.getX(),
+				character.getY(),
+				character.getWidth(),
+				character.getHeight()
+				);
+		Rectangle thisObject = new Rectangle(x, y, width, height);			
+		
+		
+		//user built-in method to check intersection (COLLISION)
+		return main.intersects(thisObject);
+	}
+	
+	//2nd constructor - allow setting x and y during consutrction
+	public Charmander(int x, int y) {
+		
+		//call the deafult constructor for all the normal stuff
+		this(); //invokes default constructor 
+		
+		//do the specific task for THIS constructor 
+		this.x = x;
+		this.y = y; 
+	}
+	
+	public void resetPosition(){
+		this.x = 300;
+		this.y = 690; 
+	}
+	
+	
+	public void move(int dir) {
+		
+		switch(dir) {
+	
+		case 0: //hop up 
+			y-= height*1.5; //move up a body-length
+						
+			
+			break;
+		case 1: //hop down
+			y += height*1.5; //move down by a body length
+			
+			break;
+			
+		case 2: // hopleft
+			x-= width*1.5;
+			
+			
+			break; 
+			
+		case 3:// hop right
+			x += width*1.5;
+		
+			break;
+		
+		
+		
+		
+		
+		}
+	
+		
+	}
+
+	
+	
+	/*
+	 * Getters!
+	 */
+	public int getX() {
+		return x;
+	}
+	
+	public int getY() {
+		return y;
+	}
+	
+	public int getWidth() {
+		return width;
+	}
+	
+	public int getHeight() {
+		return height; 
+	}
+	
+	public void setVx(int Vx) {
+		vx = Vx;
+	}
+	
+	public int getVx() {
+		
+		return vx/2;
+	}
+
+	public void paint(Graphics g) {
+		//these are the 2 lines of code needed draw an image on the screen
+		Graphics2D g2 = (Graphics2D) g;
+		
+		x+=vx;
+		y+=vy;	
+		
+		init(x,y);
+
+			g2.drawImage(forward, tx, null);
+		//draw hit box based on x, y, width, height
+		//for collision detection 
+			if(Frame.debugging) {
+				// draw hitbox only if debugging
+				g.setColor(Color.green);
+				g.drawRect(x, y, width, height);
+			}
+	}
+	
+	private void init(double a, double b) {
+		tx.setToTranslation(a, b);
+		tx.scale(scaleWidth, scaleHeight);
+	}
+
+	private Image getImage(String path) {
+		Image tempImage = null;
+		try {
+			URL imageURL = Charmander.class.getResource(path);
+			tempImage = Toolkit.getDefaultToolkit().getImage(imageURL);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return tempImage;
+	}
+	public void resetCharmander() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- FroggerStyleProject/src/Frame.java
+++ FroggerStyleProject/src/Frame.java
@@ -13,6 +13,7 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.util.ArrayList;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -21,6 +22,8 @@ import javax.swing.JPanel;
 import javax.swing.Timer;
 
 public class Frame extends JPanel implements ActionListener, MouseListener, KeyListener {
+	//for any debugging code we add public static boolean debugging = true
+	public static boolean debugging = true; 
 	
 	//Timer related variables
 	int waveTimer = 5; //each wave of enemies is 20s
@@ -31,18 +34,165 @@ public class Frame extends JPanel implements ActionListener, MouseListener, KeyL
 	
 	Font myFont = new Font("Courier", Font.BOLD, 40);
 	SimpleAudioPlayer backgroundMusic = new SimpleAudioPlayer("scifi.wav", false);
-//	Music soundBang = new Music("bang.wav", false);
-//	Music soundHaha = new Music("haha.wav", false);
+	
+	
+	Charmander charmander = new Charmander();
+	Charmander charmander2 = new Charmander(150,150);
+	SquirtleScroller squirtle = new SquirtleScroller(150, 50);
+	Background background = new Background();
+	Water water = new Water();
+	
+
+	
+	//a row of CharmanderScroller objects!
+	Water[] row1 = new Water[1];
+	CharmanderScroller[] row2 = new CharmanderScroller[5];
+	CharmanderScroller2[] row3 = new CharmanderScroller2[5];
+	SquirtleScroller[] row4 = new SquirtleScroller[8];
+	ArrayList<CharmanderScroller> row1List = new ArrayList <CharmanderScroller>();
+	ArrayList<LifeImage> lives = new ArrayList <LifeImage>();
 	
 	//frame width/height
-	int width = 600;
-	int height = 600;	
+	static int width = 600;
+	static int height = 800;	
 	
 
 	public void paint(Graphics g) {
 		super.paintComponent(g);
+		
+		
+		//paint the other objects on the screen
+		water.paint(g);
+		charmander2.paint(g);
+		squirtle.paint(g);
+		background.paint(g);
+		charmander.paint(g);
+		
+		
+		boolean riding = false;
+		 
+		/*Collision Detction */
+		//for each Ghost object in row 1 array
+		
+		for (SquirtleScroller obj : row4 ) {
+			//invoke the collided method fo ryour
+			//class - pass the main character
+			//as your arugment
+			if (obj.collided(charmander)) {
+				System.out.println("hit");
+				
+				charmander.resetPosition();
+				
+			}
+		}
+		
+		for (Water obj : row1) {
+			//invoke the collided method fo ryour
+			//class - pass the main character
+			//as your arugment
+			if (obj.collided(charmander)) {
+				System.out.println("hit");
+				
+				charmander.resetPosition();
+			}
+		}
+		
+		for (CharmanderScroller obj : row2) {
+			//invoke the collided method fo ryour
+			//class - pass the main character
+			//as your arugment
+			if (obj.collided(charmander)) {
+				
+				System.out.println("safe");
+				
+			}
+		}
+	
+		
+		for(CharmanderScroller obj: row2) {
+			if(obj.collided(charmander)) {
+				charmander.setVx(obj.getVx());
+				riding = true;
+				break;
+		
+			}
+		}
+		
+		//main character stops moving if not on rideable object
+		if(!riding && charmander.getY() > 120) {
+			riding = false; 
+			charmander.setVx(0);
+			 
+		}
+		
+		for (CharmanderScroller2 obj : row3) {
+			//invoke the collided method fo ryour
+			//class - pass the main character
+			//as your arugment
+			if (obj.collided(charmander)) {
+				System.out.println("safe");
+				riding = true;
+				break;
+		
+			}
+		}
+		
+		//main character stops moving if not on rideable object
+		if(!riding && charmander.getY() > 120) {
+			riding = false; 
+			charmander.setVx(0);
+			 
+		}
+		
+		
+		for(CharmanderScroller obj : row1List) { // for every Charmander object in row1List
+			obj.paint(g);
+		}
+		
+		//drawing the life counter images
+		for(LifeImage obj: lives) {
+			//draw the LifeImage objects
+			obj.paint(g);
+			
+			
+		}
 
+	
+		
+	
+		
+			
+	
+	
+		
+			
+		
+		
+		// have the row1 objects paint on the screen!
+		
+				// for each obj in row3
+				for(Water obj : row1) {
+					obj.paint(g);
+}
+				// for each obj in row1 
+				for(CharmanderScroller obj : row2) {
+					obj.paint(g);
+	}	
+				
+				// for each obj in row2
+				for(CharmanderScroller2 obj : row3) {
+					obj.paint(g);
+				}
+		
+				// for each obj in row3
+				for(SquirtleScroller obj : row4) {
+					obj.paint(g);
 	}
+				charmander.paint(g);
+	
+		
+		}
+		
 	
 	public static void main(String[] arg) {
 		Frame f = new Frame();
@@ -52,15 +202,46 @@ public class Frame extends JPanel implements ActionListener, MouseListener, KeyL
 	public Frame() {
 		JFrame f = new JFrame("Duck Hunt");
 		f.setSize(new Dimension(width, height));
-		f.setBackground(Color.white);
+		f.setBackground(Color.green);
 		f.add(this);
 		f.setResizable(false);
  		f.addMouseListener(this);
 		f.addKeyListener(this);
 	
-		backgroundMusic.play();
+//		backgroundMusic.play();
 
-	
+		/* 
+		 * Setup any 1D array here! - create the objects that go in them ;0
+		 */
+		
+		
+		
+		//traverse the array 
+		for(int i = 0; i < row1.length; i++) {
+			row1[i] = new Water( i*500 , 150);
+		}
+		for(int i = 0; i < row2.length; i++) {
+			row2[i] = new CharmanderScroller( i*400 , 260);
+		}
+		
+		for(int i = 0; i < row3.length; i++) {
+			row3[i] = new CharmanderScroller2( i*550 , 125);
+		}
+		
+		for(int i = 0; i < row4.length; i++) {
+			row4[i] = new SquirtleScroller( i*250 , 500);
+		}
+		
+		for(int i = 0; i < 10; i++) { 
+			//run the body 10x 
+			this.row1List.add(new CharmanderScroller(i*400, 260));
+		}
+		
+		//start with 6 attempts
+		for(int i = 0; i < 6; i++) {
+			
+			this.lives.add(new LifeImage(i*40, 10));
+		}
 		
 		//the cursor image must be outside of the src folder
 		//you will need to import a couple of classes to make it fully 
@@ -118,7 +299,22 @@ public class Frame extends JPanel implements ActionListener, MouseListener, KeyL
 	public void keyPressed(KeyEvent arg0) {
 		// TODO Auto-generated method stub
 		System.out.println(arg0.getKeyCode());
+		if(arg0.getKeyCode()==87) {
+			//move character up
+			charmander.move(0);
+		}else if (arg0.getKeyCode()==83) {
+			//move character down
+			charmander.move(1);
 		
+		}else if (arg0.getKeyCode()==65) {
+			//move character down
+			charmander.move(2);
+			
+		}else if (arg0.getKeyCode()==68) {
+			//move character down
+			charmander.move(3);
+		
+		}
 		
 		
 	}
